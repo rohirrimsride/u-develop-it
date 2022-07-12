d264a2fdd3441a7d7523a33986fb121dbe8a930a
Strider <dave.barnes23@gmail.com>
2022-07-11 20:58:05 -0500

    created the connection between the server.js and the database

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // mysql pw
+        password: 'Amuse-Pastry-Secret-Yellow-2',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 
