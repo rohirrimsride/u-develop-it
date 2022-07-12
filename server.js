b882c6c078ee2eb046480745f4ec02c0f90f19b4
Strider <dave.barnes23@gmail.com>
2022-07-11 21:19:11 -0500

    added search, create and delete functions to server.js

--- server.js
+++ server.js
@@ -19,12 +19,38 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// search all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
 
+// search individual candidates
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) =>{
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
 
+// delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 
 // Default reponse for any other request (Not Found)
